d02d74996582897db36546ee2b2cda9883a21c09
aminst <aminst79@gmail.com>
2021-03-10 15:30:05 +0330

    Add command names

--- ftpserver/src/CommandHandler.cpp
+++ ftpserver/src/CommandHandler.cpp
@@ -5,4 +5,30 @@ using namespace std;
 void CommandHandler::run_command(string input)
 {
     Request request = CommandParser::parse_request(input);
+    string command = request.get_command();
+    if (command == "user")
+        ;
+    else if (command == "pass")
+        ;
+    else if (command == "pwd")
+        ;
+    else if (command == "mkd")
+        ;
+    else if (command == "dele")
+        ;
+    else if (command == "ls")
+        ;
+    else if (command == "cwd")
+        ;
+    else if (command == "rename")
+        ;
+    else if (command == "retr")
+        ;
+    else if (command == "help")
+        ;
+    else if (command == "quit")
+        ;
+    else
+        ;
+
 }
