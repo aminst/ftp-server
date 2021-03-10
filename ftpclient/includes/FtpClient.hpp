2c529d557924357a29aaa332fd2b1183086686bb
aminst <aminst79@gmail.com>
2021-03-10 13:28:37 +0330

    Add FtpClient class

---
+++ ftpclient/includes/FtpClient.hpp
@@ -0,0 +1,20 @@
+#ifndef __FTP_CLIENT__
+#define __FTP_CLIENT__
+
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define MAX_BUFFER_SIZE 1024
+
+class FtpClient
+{
+public:
+    void start();
+};
+
+#endif
